7334c8d0128b4effa2456e4df0644176b67a1e3b
Deepali Saini <deepalisa4444@gmail.com>
2025-02-02 09:12:45 +0000

    Add bookmark button feature on problem page with dynamic image source

--- content.js
+++ content.js
@@ -1,7 +1,13 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+ const bookmarkImgURL = chrome.runtime.getURL("Assets/bookmark.png");
+window.addEventListener("load", addBookmarkButton);
+
+function addBookmarkButton(){
+    const BookmarkButton = document.createElement('img');
+    BookmarkButton.id = "add-bookmark-button";
+    BookmarkButton.src= bookmarkImgURL;
+    BookmarkButton.style.height = "30px";
+    BookmarkButton.style.width = "30px";
+
+    const askDoubtButton = document.getElementsByClassName("coding_ask_doubt_button__FjwXJ")[0];
+    askDoubtButton.insertAdjacentElement("beforebegin", BookmarkButton);
+}
