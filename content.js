e63fc5f399ef68a53fdbdcaba4b464a02d055ba6
Deepali Saini <deepalisa4444@gmail.com>
2025-02-02 11:12:48 +0000

    Add bookmark handling with unique ID extraction and Chrome storage update

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
- const bookmarkImgURL = chrome.runtime.getURL("Assets/bookmark.png");
-window.addEventListener("load", addBookmarkButton);
+const bookmarkImgURL = chrome.runtime.getURL("Assets/bookmark.png");
+const PROBLEM_KEY = "PROBLEM_KEY"; 
+ window.addEventListener("load", addBookmarkButton);
 
 function addBookmarkButton(){
     const BookmarkButton = document.createElement('img');
@@ -10,4 +11,41 @@ function addBookmarkButton(){
 
     const askDoubtButton = document.getElementsByClassName("coding_ask_doubt_button__FjwXJ")[0];
     askDoubtButton.parentNode.insertAdjacentElement("afterend", BookmarkButton);
+    BookmarkButton.addEventListener("click", addNewBookmarkHandler);
+}
+
+async function addNewBookmarkHandler(){
+    const currentBookmarks = await getCurrentBookmarks();  
+    const problemurl = window.location.href;
+    const uniqueId = extractUniqueId(problemurl); 
+    const problemName = document.getElementsByClassName("Header_resource_heading_cpRp1")[0].innerText;
+
+    if(currentBookmarks.some((bookmark) => bookmark.id === uniqueId)) return;
+
+    const bookmarkObj = {
+        id: uniqueId,
+        name: problemName,
+        url: problemurl
+    }
+
+    const updatedbookmarks = [...currentBookmarks, bookmarkObj];
+
+    chrome.storage.sync.set({PROBLEM_KEY: updatedbookmarks}, () =>{
+        console.log("updated the bookmarks correctly to", updatedbookmarks);
+    })
+}
+
+function extractUniqueId(){
+        const start = url.indexOf("problems/") + "problems/".length;
+        const end = url.indexOf("?", start);
+        return url.substring(start, end);   
+}
+
+function getCurrentBookmarks(){
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get([PROBLEM_KEY ], (results) => {
+            resolve(results[PROBLEM_KEY] || []);
+        });
+
+    })
 }
