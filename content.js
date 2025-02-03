c952140a519d09f3a683ffc3c5316eb166cab019
Deepali Saini <deepalisa4444@gmail.com>
2025-02-03 04:33:10 +0000

    Add mutation Observer

--- content.js
+++ content.js
@@ -2,7 +2,22 @@ const bookmarkImgURL = chrome.runtime.getURL("Assets/bookmark.png");
 const PROBLEM_KEY = "PROBLEM_KEY"; 
  window.addEventListener("load", addBookmarkButton);
 
+ const observer = new MutationObserver(() =>{
+    addBookmarkButton();
+ }) 
+
+ observer.observe(document.body, {childList: true, subtree: true});
+
+ addBookmarkButton();
+
+ function onProblemPage(){
+    return window.location.pathname.startsWith('/problems/');
+ }
+
 function addBookmarkButton(){
+    console.log("Trigerring"); 
+    if(!onProblemPage() || !document.getElementById("add-bookmark-button")) return ;
+    if(!onProblemPage())return;
     const BookmarkButton = document.createElement('img');
     BookmarkButton.id = "add-bookmark-button";
     BookmarkButton.src= bookmarkImgURL;
@@ -18,7 +33,7 @@ async function addNewBookmarkHandler(){
     const currentBookmarks = await getCurrentBookmarks();  
     const problemurl = window.location.href;
     const uniqueId = extractUniqueId(problemurl); 
-    const problemName = document.querySelector("h4.problem_heading").textContent
+    const problemName = document.querySelector("h4.problem_heading").textContent;
 
     if(currentBookmarks.some((bookmark) => bookmark.id === uniqueId)) return;
 
